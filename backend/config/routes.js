d099fb96a354f1e6cdc5eaa53ab19d6aa3216afd
havok1305 <havok1305@gmail.com>
2017-06-21 17:19:26 -0300

    Alteradas algumas mensagens padrões de validação. Configurada rota para o serviço BillingCycle

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -3,4 +3,8 @@ const express = require('express')
 module.exports = function(server){
     //API routes
     const router = express.Router()
+    server.use('/api', router)
+
+    const billingCycleService = require('../api/billingCycle/billingCycleService')
+    billingCycleService.register(router, '/billingCycles')
 }
