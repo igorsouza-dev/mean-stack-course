2a554972b5f6fd7c71c33f44e2d59394ddee837d
havok1305 <havok1305@gmail.com>
2017-06-21 14:26:42 -0300

    Configurando mongodb

---
+++ backend/config/server.js
@@ -0,0 +1,16 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+/***setup the middlewares that will be used by the requests***/
+
+//encode URL
+server.use(bodyParser.urlencoded({ extended : true }))
+//parse json into object
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+    console.log(`BACKEND is running on port ${port}.`)
+})
