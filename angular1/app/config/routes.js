50ea8b15343a0ec33d6792ce152720fde0892972
havok1305 <havok1305@gmail.com>
2017-06-22 17:32:45 -0300

    Alterados templates

---
+++ angular1/app/config/routes.js
@@ -0,0 +1,16 @@
+angular.module('primeiraApp').config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+        $stateProvider.state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "dashboard/dashboard.html"
+        }).state('billingCycle', {
+            url: "/billingCycles", 
+            templateUrl: "billingCycle/tabs.html"
+        })
+
+        //in case it doesnt find any url, return to the dashboard
+        $urlRouterProvider.otherwise('/dashboard')
+    }
+])
