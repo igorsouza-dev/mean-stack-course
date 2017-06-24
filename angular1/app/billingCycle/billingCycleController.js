737cd5a86b8f3c386b6643b7f0f9cb16910c54fc
Igor Martins <havok1305@gmail.com>
2017-06-24 16:13:02 -0300

    Adicionados campos para créditos e débitos. Adicionado sumário

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -11,7 +11,7 @@
 
         vm.refresh = function() {
             $http.get(url).then(function(response){
-               vm.billingCycle = {};
+               vm.billingCycle = {credits: [{}], debts: [{}]};
                vm.billingCycles = response.data;
                tabs.show(vm, {tabList: true, tabCreate: true});
             });
@@ -52,6 +52,34 @@
             }).catch(function(response){
                 msgs.addError(response.data.errors);
             });
+        };
+
+        vm.addCredit = function(index) {
+            vm.billingCycle.credits.splice(index + 1, 0, {});
+        };
+
+        vm.cloneCredit = function(index, {name, value}) {
+            vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+        };
+        
+        vm.deleteCredit = function (index) {
+            if(vm.billingCycle.credits.length > 1){
+                vm.billingCycle.credits.splice(index, 1);
+            }
+        };
+
+        vm.addDebt = function(index) {
+            vm.billingCycle.debts.splice(index + 1, 0, {});
+        };
+
+        vm.cloneDebt = function(index, {name, value}) {
+            vm.billingCycle.debts.splice(index + 1, 0, {name, value})
+        };
+
+        vm.deleteDebt = function (index) {
+            if(vm.billingCycle.debts.length > 1){
+                vm.billingCycle.debts.splice(index, 1);
+            }
         };
 
         vm.refresh();
