3477b25b3395779c247274041cb1bd35d0811dd2
havok1305 <havok1305@gmail.com>
2017-06-23 22:05:36 -0300

    Adicionado controle de abas

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -2,20 +2,30 @@
     angular.module("primeiraApp").controller("BillingCycleCtrl", [
         '$http',
         'msgs',
+        'tabs',
         BillingCycleController
     ]);
-    function BillingCycleController($http, msgs) {
+    function BillingCycleController($http, msgs, tabs) {
         const vm = this;
+        const url = "http://localhost:3003/api/billingCycles";
+
+        vm.refresh = function() {
+            $http.get(url).then(function(response){
+               vm.billingCycle = {};
+               vm.billingCycles = response.data;
+               tabs.show(vm, {tabList: true, tabCreate: true});
+            });
+        };
 
         vm.create = function(){
-            const url = "http://localhost:3003/api/billingCycles";
             $http.post(url, vm.billingCycle).then(function (response) {
-                vm.billingCycle = {};
+                vm.refresh();
                 msgs.addSuccess("Operação realizada com sucesso!");
             }).catch(function(response){
                 msgs.addError(response.data.errors);
             });
         };
 
+        vm.refresh();
     }
 })();
