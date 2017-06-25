6729cc2d0f0ebee32216b04fffba1ebd18082ecb
Igor Martins <havok1305@gmail.com>
2017-06-25 10:37:41 -0300

    Adicionando paginação

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,28 +1,28 @@
-const BillingCycle = require('./billingCycle')
-const _ = require('lodash')
+const BillingCycle = require('./billingCycle');
+const _ = require('lodash');
 
-BillingCycle.methods(['get','post','put','delete'])
-BillingCycle.updateOptions({new: true, runValidators: true})//return the new object after the update
+BillingCycle.methods(['get','post','put','delete']);
+BillingCycle.updateOptions({new: true, runValidators: true});//return the new object after the update
 
-BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext);
 
 //function to override the objects passed when errors happen
 function sendErrorsOrNext(req, res, next) {
-    const bundle = res.locals.bundle
+    const bundle = res.locals.bundle;
     if(bundle.errors){
-        var errors = parseErrors(bundle.errors)
-        res.status(500).json({errors})
+        var errors = parseErrors(bundle.errors);
+        res.status(500).json({errors});
     }else{
         //if there is no errors, go to the next
-        next()
+        next();
     }
 }
 
 function parseErrors(nodeRestfulErrors) {
-    const errors = []
+    const errors = [];
     //iterate through the array of errors and get only the message, and then it pushes the message to the final array of errors
-    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
-    return errors
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message));
+    return errors;
 }
 
 BillingCycle.route('count', function(req, res, next){
@@ -31,12 +31,12 @@ BillingCycle.route('count', function(req, res, next){
         //callback function 
         if(error){
             //in case of error
-            res.status(500).json({errors: [error]})
+            res.status(500).json({errors: [error]});
         } else {
             //send result
-            res.json({valule})
+            res.json({value});
         }
     })
-})
+});
 
-module.exports = BillingCycle
+module.exports = BillingCycle;
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,20 +1,21 @@
-const port = 3003
+const port = 3003;
 
-const bodyParser = require('body-parser')
-const express = require('express')
-const server = express()
-const allowCors = require('./cors')
-
+const bodyParser = require('body-parser');
+const express = require('express');
+const server = express();
+const allowCors = require('./cors');
+const queryParser = require('express-query-int');
 /***setup the middlewares that will be used by the requests***/
 
 //encode URL
-server.use(bodyParser.urlencoded({ extended : true }))
+server.use(bodyParser.urlencoded({ extended : true }));
 //parse json into object
-server.use(bodyParser.json())
-server.use(allowCors)
+server.use(bodyParser.json());
+server.use(allowCors);
+server.use(queryParser());
 
 server.listen(port, function(){
-    console.log(`BACKEND is running on port ${port}.`)
-})
+    console.log(`BACKEND is running on port ${port}.`);
+});
 
-module.exports = server
+module.exports = server;
