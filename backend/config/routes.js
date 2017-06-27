5fdf334a408848d1bb1e038682f0c7d2fd573661
havok1305 <havok1305@gmail.com>
2017-06-26 21:22:57 -0300

    Adicionada autenticação do backend

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,13 +1,31 @@
-const express = require('express')
+const express = require('express');
+const auth = require('./auth');
 
 module.exports = function(server){
-    //API routes
-    const router = express.Router()
-    server.use('/api', router)
 
-    const billingCycleService = require('../api/billingCycle/billingCycleService')
-    billingCycleService.register(router, '/billingCycles')
+    /*
+       * Rotas abertas
+       */
+    const openApi = express.Router();
+    server.use('/oapi', openApi);
 
-    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
-    router.route('/billingSummary').get(billingSummaryService.getSummary)
-}
+    const AuthService = require('../api/user/authService');
+    openApi.post('/login', AuthService.login);
+    openApi.post('/signup', AuthService.signup);
+    openApi.post('/validateToken', AuthService.validateToken);
+
+    /*
+       * Rotas protegidas por Token JWT
+       */
+    const protectedApi = express.Router();
+    server.use('/api', protectedApi);
+
+    protectedApi.use(auth);
+
+    const billingCycleService = require('../api/billingCycle/billingCycleService');
+    billingCycleService.register(protectedApi, '/billingCycles');
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService');
+    protectedApi.route('/billingSummary').get(billingSummaryService.getSummary);
+
+};
